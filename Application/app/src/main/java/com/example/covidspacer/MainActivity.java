e87893744118c1059eec1f68d4634cd80ae9d470
rhaegarii <wakaflockaflaim@gmail.com>
2020-05-28 12:41:42 -0400

    added basic xml and imported libraries

--- Application/app/src/main/java/com/example/covidspacer/MainActivity.java
+++ Application/app/src/main/java/com/example/covidspacer/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.covidspacer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import mobi.inthepocket.android.beacons.ibeaconscanner.Beacon;
+import mobi.inthepocket.android.beacons.ibeaconscanner.IBeaconScanner;
+
+
 
 import android.os.Bundle;
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IBeaconScanner.initialize(IBeaconScanner.newInitializer(this)
+                .setTargetService(BeaconHandler.class)
+                .build());
+
+        final Beacon beacon = Beacon.newBuilder()
+                .setUUID("DDDD98FF-2900-441A-802F-9C398FC1DDDD")
+                .setMajor(1)
+                .setMinor(2)
+                .build();
     }
 }
